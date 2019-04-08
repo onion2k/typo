1560fc15059cf6201db90f8bc9a46da5cd45bd9a
Chris Neale <chris@komododigital.co.uk>
2019-04-08 08:52:07 +0100

    Added new settings:

--- src/Controller.js
+++ src/Controller.js
@@ -1,30 +1,69 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 
-import './Controller.css';
+import "./Controller.css";
 
 export default function Controller(data) {
-    
-  const updateColor = (e) => {
-    data.update({...data.style, color: e.target.value});
+  const update = e => {
+    const style = { ...data.style };
+    style[e.target.name] = e.target.value;
+    data.update({ ...style });
+  };
 
-  }
+  const updateColor = e => {
+    data.update({ ...data.style, color: e.target.value });
+  };
 
-  const updateSize = (e) => {
-    data.update({...data.style, fontSize: e.target.value});
-  }
+  const updateSize = e => {
+    data.update({ ...data.style, fontSize: e.target.value });
+  };
 
-  const updateFamily = (e) => {
-    data.update({...data.style, fontFamily: e.target.value});
-  }
-  
+  const updateFamily = e => {
+    data.update({ ...data.style, fontFamily: e.target.value });
+  };
+
+  const cssTextFeatures = [
+    { name: "letterSpacing", label: "Letter Spacing" },
+    { name: "lineHeight", label: "Line Height" }
+  ];
+
+  const settings = cssTextFeatures.map(s => {
+    return (
+      <>
+        <label htmlFor={s.name}>{s.label}</label>
+        <input
+          name={s.name}
+          type="text"
+          onChange={update}
+          defaultValue={data.style[s.name] || "inherit"}
+        />
+      </>
+    );
+  });
+
   return (
     <div className="controller">
+      {settings}
       <label htmlFor="color">Color</label>
-      <input name="color" type="text" onChange={updateColor} defaultValue={data.style.color} />
+      <input
+        name="color"
+        type="text"
+        onChange={updateColor}
+        defaultValue={data.style.color}
+      />
       <label htmlFor="size">Size</label>
-      <input name="size" type="text" onChange={updateSize} defaultValue={data.style.fontSize} />
+      <input
+        name="size"
+        type="text"
+        onChange={updateSize}
+        defaultValue={data.style.fontSize}
+      />
       <label htmlFor="family">Font Family</label>
-      <input name="family" type="text" onChange={updateFamily} defaultValue={data.style.fontFamily} />
+      <input
+        name="family"
+        type="text"
+        onChange={updateFamily}
+        defaultValue={data.style.fontFamily}
+      />
     </div>
   );
 }
--- src/Typo.js
+++ src/Typo.js
@@ -1,28 +1,33 @@
-import React, { useContext } from 'react';
-import SplitContext from './Split';
+import React, { useContext } from "react";
+import SplitContext from "./Split";
 
-import './Typo.css';
+import "./Typo.css";
 
 export default function Typo(data) {
   let split = useContext(SplitContext);
-  
-  let clipPath = '';
+
+  let clipPath = "";
   let style = {};
 
   if (data.copy) {
-    clipPath = `polygon(${split.split}px 0, 100vw 0, 100vw 100vh, ${split.split}px 100vh)`;
+    clipPath = `polygon(${split.split}px 0, 100vw 0, 100vw 100vh, ${
+      split.split
+    }px 100vh)`;
   }
-  
+
   if (data.style) {
     style.color = data.style.color;
     style.fontSize = data.style.fontSize;
     style.fontFamily = data.style.fontFamily;
+    style.letterSpacing = data.style.letterSpacing;
+    style.lineHeight = data.style.lineHeight;
   }
 
   return (
     <div
-    className={`typo ${data.copy?"copy":"original"}`}
-    style={{ clipPath }}>
+      className={`typo ${data.copy ? "copy" : "original"}`}
+      style={{ clipPath }}
+    >
       <span className="title">{data.title}</span>
       <div className="content" style={style}>
         Ooer
