f11e9dcf31989b1d35cc5a49f393bdf6a9173ab3
Chris Neale <chris@komododigital.co.uk>
2019-04-12 12:37:26 +0100

    Added dropzone for local font loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
 import SplitContext from "./Split";
 import Typo from "./Components/Typo";
 import Controller from "./Components/Controller";
@@ -20,6 +21,25 @@ export default function App() {
   let [content, setContent] = useState(defaultContent);
   let [original, setOriginal] = useState(defaultState);
   let [copy, setCopy] = useState(defaultState);
+
+  const onDrop = useCallback(acceptedFiles => {
+    const reader = new FileReader();
+
+    reader.onabort = () => console.log("file reading was aborted");
+    reader.onerror = () => console.log("file reading has failed");
+    reader.onload = file => {
+      // Do whatever you want with the file contents
+      const binaryStr = reader.result;
+      console.log(file, binaryStr);
+    };
+
+    acceptedFiles.forEach(file => reader.readAsBinaryString(file));
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop
+  });
+
   const transfer = (name, value) => {
     const originalStyle = { ...original };
     originalStyle[name] = value;
@@ -28,8 +48,10 @@ export default function App() {
     copyStyle[name] = value;
     setCopy({ ...copyStyle });
   };
+
   return (
-    <div className="App">
+    <div className={`App ${isDragActive ? "drag" : ""}`} {...getRootProps()}>
+      <input {...getInputProps()} />
       <Header setContent={setContent} />
       <SplitContext.Provider value={{ split: split, update: setSplit }}>
         <div className="typo-splitscreen">
--- src/Components/Controller.js
+++ src/Components/Controller.js
@@ -31,15 +31,29 @@ export default function Controller(data) {
               </option>
             );
           })}
-          <option key={"inherit"} value={"inherit"}>{"inherit"}</option>
-          <option key={"initial"} value={"initial"}>{"initial"}</option>
-          <option key={"unset"} value={"unset"}>{"unset"}</option>
+          <option key={"inherit"} value={"inherit"}>
+            {"inherit"}
+          </option>
+          <option key={"initial"} value={"initial"}>
+            {"initial"}
+          </option>
+          <option key={"unset"} value={"unset"}>
+            {"unset"}
+          </option>
         </select>
       );
     }
     return (
       <React.Fragment key={s.name}>
-        <label htmlFor={s.name}><a target="_BLANK" href={`https://developer.mozilla.org/en-US/docs/Web/CSS/${s.label}`}>{s.label}</a></label>
+        <label htmlFor={s.name}>
+          <a
+            target="_BLANK"
+            href={`https://developer.mozilla.org/en-US/docs/Web/CSS/${s.label}`}
+            rel="noopener noreferrer"
+          >
+            {s.label}
+          </a>
+        </label>
         {input}
         <button onClick={() => data.transfer(s.name, data.style[s.name])}>
           ⇌
