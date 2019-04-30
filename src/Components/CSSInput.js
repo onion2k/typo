a0f4bf26f356fd7b0dccdfb0f20d6a24d4b9acfd
Chris Neale <chris@usablehq.com>
2019-04-30 20:06:33 +0100

    Added mousewheel support to CSS input

--- src/Components/CSSInput.js
+++ src/Components/CSSInput.js
@@ -24,20 +24,33 @@ export default function CSSInput({ name, value, type, placeholder, update}) {
   const onKeyUp = (e) => {
     if (e.keyCode === 38 || e.keyCode === 40) {
       let mod = 1;
-
       if (e.keyCode === 40) {
         mod = -1;
       }
-
-      let [ val, whole, quantity, fraction, unit ] = value.match(length);
-      if (fraction) {
-        whole = parseFloat(whole)+(mod*0.1);
-      } else {
-        whole = parseFloat(whole)+mod;
-      }
-      setStateValue(`${whole}${unit}`);
+      updateStateValue(mod);
     }
 
+  }
+
+  const onWheel = (e) => {
+    e.preventDefault();
+    let mod = 1;
+    if (e.deltaY > 0) {
+      mod = -1;
+    }
+    updateStateValue(mod);
+
+  }
+  
+  const updateStateValue = (mod) => {
+    let [ val, whole, quantity, fraction, unit ] = value.match(length);
+    if (fraction) {
+      whole = parseFloat(whole)+(mod*0.1);
+      whole = whole.toFixed(1);
+    } else {
+      whole = parseFloat(whole)+mod;
+    }
+    setStateValue(`${whole}${unit}`);
   }
   
   const onChange = e => {
@@ -48,6 +61,7 @@ export default function CSSInput({ name, value, type, placeholder, update}) {
     <input
       ref={ref}
       onKeyUp={onKeyUp}
+      onWheel={onWheel}
       onChange={onChange}
       type="text"
       name={name}
